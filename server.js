7ad55602315656ee6244182b096e4987a5755dcf
Ravinder2301 <rav23inder@gmail.com>
2024-02-21 17:01:09 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,138 @@
+// Import necessary modules
+import express from 'express'; // Express.js framework
+import mongoose from 'mongoose';
+import cors from 'cors'; // Cross-Origin Resource Sharing middleware
+import jwt from 'jsonwebtoken'; // JSON Web Token implementation
+import bcrypt from 'bcryptjs'; // Password hashing library
+import cookieParser from 'cookie-parser'; // Parse cookies
+import dotenv from 'dotenv'; // Load environment variables from a .env file
+import { User } from './DBSchema/schema.js';
+dotenv.config(); // Initialize dotenv
+
+// Initialize Express app
+const app = express();
+const URL = process.env.MONGO_URL
+
+// Middleware
+app.use(express.json()); // Parse JSON bodies
+app.use(cors({
+    origin: ["http://localhost:5173"], // Allow requests from this origin
+    methods: ["POST", "GET"], // Allow specified HTTP methods
+    credentials: true // Allow sending cookies
+}));
+app.use(cookieParser()); // Parse cookies
+
+// Database connection
+mongoose.connect(URL)
+.then(() => {
+    console.log("App connected to database")
+})
+.catch((err) => {
+    console.log(err)
+});
+
+// Middleware to verify user authentication
+const verifyUser = (req, res, next) => {
+    const token = req.cookies.token;
+    if(!token){
+        return res.json({Error: "You are not authenticated"})
+    }else{
+        jwt.verify(token, "jwt-secret-key", (err, decoded) => {
+            if(err) {
+                return res.json({Error: "token is not correct"})
+            }else{
+                req.name = decoded.name;
+                next();
+            }
+        })
+    }
+}   
+
+// Route: Home page (requires authentication)
+app.get('/', verifyUser, (req, res) => {
+    return res.json({ Status: "Success", name: req.name });
+})
+
+// Route: User registration
+app.post('/register', async (req, res) => {
+    try {
+      const { name, email, password } = req.body;
+      
+      // Check if all required fields are provided
+      if (!name || !email || !password) {
+        return res.json({ Error: "Send all required fields!" });
+      }
+  
+      // Check if email already exists in the database
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.json({ Error: "Email already exists!" });
+      }
+  
+      // Hash the password
+      const hashedPassword = await bcrypt.hash(password, 10);
+  
+      // Create a new user
+      const newUser = await User.create({ name, email, password: hashedPassword });
+    //   await newUser.save();
+  
+      return res.json({ Status: "Success" });
+    } catch (err) {
+      console.log(err);
+      return res.json({ Error: "Registration error in server" });
+    }
+  });
+  
+
+// Route: User login
+app.post('/login', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      
+      // Check if email and password are provided
+      if (!email || !password) {
+        return res.json({ Error: "Send all required fields!" });
+      }
+  
+      // Check if user with provided email exists in the database
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.json({ Error: "No user with that email exists" });
+      }
+  
+      // Compare passwords
+      const passwordMatch = await bcrypt.compare(password, user.password);
+      if (passwordMatch) {
+        // Generate JWT token
+        const token = jwt.sign({ name: user.name }, process.env.JWT_SECRET, { expiresIn: '1d' });
+        
+        // Set token in cookie
+        res.cookie('token', token, { httpOnly: true });
+  
+        return res.json({ Status: "Success" });
+      } else {
+        return res.json({ Error: "Password incorrect" });
+      }
+    } catch (err) {
+      console.log(err);
+      return res.json({ Error: "Login error in server" });
+    }
+  });
+  
+
+// Route: User logout
+app.get('/logout', (req, res) => {
+    try {
+      res.clearCookie('token'); // Clear JWT token from cookie
+      return res.json({ Status: "Success" });
+    } catch (err) {
+      console.log(err);
+      return res.json({ Error: "Logout error in server" });
+    }
+  });
+  
+
+// Start the server
+app.listen(process.env.PORT, () => {
+    console.log(`Port running on : ${process.env.PORT} `)
+})
